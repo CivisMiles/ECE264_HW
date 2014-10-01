5c8dd93add1bb8da6966908602168f4b0a377ed4
Benjamin Paul Frazier <bfrazie@ecegrid-thin1.ecn.purdue.edu>
2014-09-30 21:04:41 -0400

    modified:   answer02.c

--- PA02/answer02.c
+++ PA02/answer02.c
@@ -52,7 +52,7 @@ char * my_strchr(const char * str, int ch)
 char * my_strrchr(const char * str, int ch)
 {
   //Local Declarations
-  int len; //length of the string
+  //int len; //length of the string
   
   
   //Statements
@@ -66,65 +66,12 @@ char * my_strrchr(const char * str, int ch)
 char * my_strstr(const char * haystack, const char * needle)
 {
   //Local Declarations
-  char dest[50] = "(null)"
-  int lcv1 = 0; //loop control variable 1
-  int lcv2 = 0; //loop control variable 2
-  int nlen;
-  int hlen;
-  
-  //Statements
-  if (needle[0] != 0)
-  {
-    nlen = my_strlen(needle);
-    hlen = my_strlen(haystack);
-	nlen--;
-    
-	while (lcv1 < hlen && lcv2 <= nlen)
-	{
-      if (haystack[lcv1] == needle[lcv2])
-	  {
-        lcv2++;
-	  }
-	  else
-	  {
-	    lcv2 = 0;
-      }
-	  lcv1++;
-	}
-    
-	lcv2 = 0;
-
-	while (lcv1 < hlen)
-	{
-      dest[lcv2] = haystack[lcv1];
-	  lcv1++; lcv2++;
-	}
-    
-  }
-  
-  else if (needle[0] == 0)
-  {
-    while (haystack[lcv1] !=0)
-	{
-      d[lcv] = haystack[lcv1];
-	  lcv1++;
-	}
-  d[lcv1] = 0;
-  }
-  
-  return ();
-}
-
-
-char * my_strcpy(char * dest, const char * src)
-{
-  //Local Declarations
-  
+  char * r = &haystack[0]; 
   
   //Statements
   
   
-  return (EXIT_SUCCESS);
+  return (*r);
 }
 
 
@@ -143,12 +90,23 @@ char * my_strcat(char * dest, const char * src)
 int my_isspace(int ch)
 {
   //Local Declarations
-  
-  
+  int lcv = 0;
+  int len;
+  int result = 0;
+
   //Statements
-  
-  
-  return (EXIT_SUCCESS);
+  len = my_strlen(ch)
+
+  while (lcv <= len)
+  {
+    if (ch[lcv] == "\t" || ch[lcv] == "\v" || ch[lcv] == "\f" || ch[lcv] == "\n" || ch[lcv] == "\r" || ch[lcv] == " ")
+	{
+      result = 1;
+	}
+  }
+
+
+ return (result);
 }
 
 
