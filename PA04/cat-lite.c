fbc796ec307553271993f20c773d0e4a86eea9a4
Benjamin Paul Frazier <bfrazie@ecegrid-thin6.ecn.purdue.edu>
2014-10-03 18:53:19 -0400

    modified:   cat-lite.c

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -13,9 +13,13 @@ int main(int argc, char * * argv)
   {
     if (strcmp(argv[lcv],"--help") == 0)
 	{
-	  printf(""
-	         ""
-			 ""
+	  printf("Usage: cat-lite [--help] [FILE]...\n"
+	         "With no FILE, or when FILE is -, read standard input\n"
+			 "\nExamples:\n"
+			 "\tcat-lite README\tPrint the file README to standard output.\n"
+			 "\tcat-lite f - g\tPrint f's contents,"
+			 " then standard input,\n\t\t\tthen g's contents.\n"
+			 "cat-lite\t\t COpy standard input to standard output.\n"
 			 "\n");
 
       return EXIT_SUCCESS;
