f8a36a5e17703b682e25fe194eb59fba696fc934
Benjamin Paul Frazier <bfrazie@ecegrid-thin6.ecn.purdue.edu>
2014-10-03 18:42:01 -0400

    modified:   cat-lite.c

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(int argc, char * * argv)
 {
   //Initial Declarations
   int lcv;
   int run = 0;
   
   //Statements
-  for (lcv = 1;lcv < argc:lcv++);
+  for (lcv = 1;lcv < argc;lcv++);
   {
     if (strcmp(argv[lcv],"--help") == 0)
 	{
 	  printf(""
 	         ""
 			 ""
-			 "");
+			 "\n");
 
       return EXIT_SUCCESS;
 	}
--- PA04/echo-lite.c
+++ PA04/echo-lite.c
@@ -6,30 +6,14 @@ int main(int argc, char * * argv )
 {
   //Initial Declarations
   int lcv;
-  int run;
-  int len = 0;
 
   //Statements
   for (lcv = 1;lcv < argc;lcv++)
   {
-    len = len + strlen(argv[lcv]);   
+    printf("%s ",argv[lcv]);
   }
- 
-  char * str = malloc(sizeof(char *) * (len + argc));
-    
-  for (lcv = 1;lcv < argc;lcv++)
-  {
-    strcat(str,argv[lcv]);
-
-    if (lcv < argc - 1)
-    {
-	  strcat(str," ");
-	}
-  } 
   
-  printf("%s\n",str);
-  
-  free (str);
+  printf("\n");
   
   return EXIT_SUCCESS;
 }
