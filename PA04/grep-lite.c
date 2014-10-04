08112d65afe631904d91742a8d2d08e761b83f57
Benjamin Paul Frazier <bfrazie@ecegrid-thin6.ecn.purdue.edu>
2014-10-03 23:43:01 -0400

    modified:   grep-lite.c

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -13,34 +13,45 @@ int main(int argc, char * * argv)
   int ind = 1;
   
   //Statements
-  if (strcmp(argv[ind],"--help") == 0)
+  for ( ;ind < argc;ind++)
   {
-    printf("\nUsage: grep-lite [OPTION]... PATTERN\n"
-           "Search for PATTERN in standard input. PATTERN is a\n"
-		   "string. grep-lite will search standard input line by\n"
-		   "line, and (by default) print out those lines which\n"
-		   "contain PATTERN as a substring.\n"
-	 	   "\n\t-v, --invert-match\tprint non-matching lines\n"
-	 	   "\t-n, --line-number\tprint line numbers with output\n"
-	 	   "\t-q, --quiet\t\tsuppress all output\n"
-	 	   "\nExit status is 0 if any line is selected, 1 otherwise;"
-		   "\nif any error occurs, then the exit status is 2.\n"
-	 	   "\n");
+    if (strcmp(argv[ind],"--help") == 0)
+    {
+      printf("\nUsage: grep-lite [OPTION]... PATTERN\n"
+             "Search for PATTERN in standard input. PATTERN is a\n"
+		     "string. grep-lite will search standard input line by\n"
+		     "line, and (by default) print out those lines which\n"
+		     "contain PATTERN as a substring.\n"
+	 	     "\n\t-v, --invert-match\tprint non-matching lines\n"
+	 	     "\t-n, --line-number\tprint line numbers with output\n"
+	 	     "\t-q, --quiet\t\tsuppress all output\n"
+	 	     "\nExit status is 0 if any line is selected, 1 otherwise;"
+		     "\nif any error occurs, then the exit status is 2.\n"
+	 	     "\n");
        
-    return EXIT_SUCCESS;
+      return EXIT_SUCCESS;
+    }
+  }
+  if (argv[argc - ind][ind] == '-')
+  {
+    fprintf(stderr,"Error:\n\tThe last input must be a pattern to"
+ 	   " search by,\n\tit may not start with a hyphen \"-\"."
+  	   "\n\taborting...\n");
+    return FAIL;
   }
   else
   {
-    for ( ;ind < argc;ind++)
+    for (ind = 1 ;ind < argc;ind++)
     {
- 	  if (argv[argc - ind][ind] == '-')
+      if (strcmp(argv[ind],"-v") == 0 || strcmp(argv[ind],"--invert-match") == 0)
 	  {
-   	    fprintf(stderr,"Error:\n\tThe last input must be a pattern to"
-	 	   " search by,\n\tit may not start with a hyphen \"-\"."
-	  	   "\n\taborting...\n");
-   	    return FAIL;
+	  
 	  }
-      else if (1)
+	  else if (strcmp(argv[ind],"-n") == 0 || strcmp(argv[ind],"--line-number") == 0)
+	  {
+	  
+	  }
+	  else if (strcmp(argv[ind],"-q") == 0 || strcmp(argv[ind],"--quiet") == 0)
 	  {
 	    
   	  }
