437c5ce7b820e12c4f55f19ddbac4ff91cd7dfc7
Benjamin Paul Frazier <bfrazie@ecegrid-thin2.ecn.purdue.edu>
2014-10-10 00:52:22 -0400

    new file:   pa05.c

---
+++ PA05/pa05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "answer05.h"
+
+int main(int argc, char * * argv)
+{
+  //Initial Declarations
+
+  //Statements
+  //testing partitionAll
+  
+  
+  //testing partitionIncreasing
+  
+  
+  //testing partitionDecreasing
+  
+  
+  //testing partitionOdd
+  
+  
+  //testing partitionEven
+  
+  
+  //testing partitionOddAndEven
+  
+  
+  //testing partitionPrime
+  
+  
+  return EXIT_SUCCESS;
+}
+
